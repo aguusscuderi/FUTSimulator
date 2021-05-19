d4f0332d64cc9eece86067523d7657d10d18b60d
aguusscuderi <aguusscuderi9@gmail.com>
2021-05-19 11:40:19 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+
+// Inicio del programa, recuperacion/indagacion de datos, creacion de objetos.
+
+let cantidad = 2;
+
+function plantelInicial (nombre, apellido, posicion, piernaHabil) {
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.posicion=posicion;
+    this.piernaHabil=piernaHabil
+}
+
+const arrayJugadores = [];
+
+function datosJugador() {
+    do {
+        alert("Bienvenido a FUTSimulator! Crea a 5 de tus 11 jugadores.")
+        arrayJugadores.push(new plantelInicial(
+            prompt("Nombre del jugador"),
+            prompt("Apellido del jugador"),
+            prompt("Posicion del jugador"),
+            prompt("Pierna habil del jugador. (D, I, Ambidiestro)"), 
+            )
+    )}while(arrayJugadores.length!=cantidad)
+
+    let jugadorJSON = JSON.stringify(arrayJugadores);
+    localStorage.setItem('datos', jugadorJSON) 
+}
+
+function recuperar() {
+    let recuperacion = JSON.parse(localStorage.getItem('datos'));
+    recuperacion.forEach(element => {
+        arrayJugadores.push(element)
+    });
+}
+
+
+function inicio() {
+    if (localStorage.datos) {
+        let aviso = confirm("Hay datos en la base, desea recuperarlos?");
+        if (aviso==true) {
+            recuperar()
+        }else {
+            datosJugador()
+        }
+        
+    }else {
+        datosJugador()
+    }
+}
+
+inicio()
+
+
+console.table(arrayJugadores)
+
