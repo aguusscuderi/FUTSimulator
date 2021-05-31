eb94389115b54489f1a875d387a6a7e4654bc83f
aguusscuderi <aguusscuderi9@gmail.com>
2021-05-31 01:44:14 -0300

    captains text hover

--- js/app.js
+++ js/app.js
@@ -52,3 +52,83 @@ function inicio() {
 inicio()
 
 
+
+//Eventos
+
+//MESSI
+const messi = document.querySelector('#messi')
+
+const selectedMessi = () => {
+    const div = document.querySelector('#messi-div')
+    let beneficio = document.createElement('p')
+    beneficio.setAttribute('class', 'beneficio')
+    beneficio.innerHTML = 'Elegir a MESSI otorga a tus jugadores un 20% mas de AGILIDAD'
+    div.appendChild(beneficio)
+}
+
+ const unselectedMessi = () => {
+     const beneficioDelete = document.querySelector('.beneficio')
+     beneficioDelete.remove()
+ }
+
+messi.addEventListener('mouseenter', selectedMessi)
+messi.addEventListener('mouseleave', unselectedMessi)
+
+//RONALDO
+const ronaldo = document.querySelector('#ronaldo')
+
+const selectedRonaldo = () => {
+    const div = document.querySelector('#ronaldo-div')
+    let beneficio = document.createElement('p')
+    beneficio.setAttribute('class', 'beneficio')
+    beneficio.innerHTML = 'Elegir a RONALDO otorga a tus jugadores un 20% mas de VELOCIDAD'
+    div.appendChild(beneficio)
+}
+
+ const unselectedRonaldo = () => {
+     const beneficioDelete = document.querySelector('.beneficio')
+     beneficioDelete.remove(ronaldo)
+ }
+
+
+ronaldo.addEventListener('mouseenter', selectedRonaldo)
+ronaldo.addEventListener('mouseleave', unselectedRonaldo)
+
+//VANDIJK
+const vandijk = document.querySelector('#vandijk')
+
+const selectedVandijk = () => {
+    const div = document.querySelector('#vandijk-div')
+    let beneficio = document.createElement('p')
+    beneficio.setAttribute('class', 'beneficio')
+    beneficio.innerHTML = 'Elegir a VANDIJK otorga a tus jugadores un 20% mas de FUERZA'
+    div.appendChild(beneficio)
+}
+
+ const unselectedVandijk = () => {
+     const beneficioDelete = document.querySelector('.beneficio')
+     beneficioDelete.remove()
+ }
+
+vandijk.addEventListener('mouseenter', selectedVandijk)
+vandijk.addEventListener('mouseleave', unselectedVandijk)
+
+//ALISSON
+const alisson = document.querySelector('#alisson')
+
+const selectedAlisson = () => {
+    const div = document.querySelector('#alisson-div')
+    let beneficio = document.createElement('p')
+    beneficio.setAttribute('class', 'beneficio')
+    beneficio.innerHTML = 'Elegir a ALISSON otorga a tus jugadores un 20% mas de REFLEJOS'
+    div.appendChild(beneficio)
+}
+
+ const unselectedAlisson = () => {
+     const beneficioDelete = document.querySelector('.beneficio')
+     beneficioDelete.remove()
+ }
+
+alisson.addEventListener('mouseenter', selectedAlisson)
+alisson.addEventListener('mouseleave', unselectedAlisson)
+
