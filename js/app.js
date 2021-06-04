86bc890fc9f4928e7072926783edd1625a5537ff
aguusscuderi <aguusscuderi9@gmail.com>
2021-06-04 09:32:26 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -53,17 +53,51 @@ inicio()
 
 
 
-//Eventos
 
-//MANIPULACION DE CAPITANES Y FUNCIONES
+    //EVENTOS 
+/////////////////////////////////////////////////
+// CREACION DINAMICA DE CAPITANES MEDIANTE ARRAY //
+////////////////////////////////////////////////
+
+let captainsArray = [
+    {id: 1, nombre: 'LIONEL', apellido: 'messi', img: 'imagenes/messiCap.png'},
+    {id: 2, nombre: 'CRISTIANO', apellido: 'ronaldo', img: 'imagenes/ronaldoCap.png'},
+    {id: 3, nombre: 'VIRGIL', apellido: 'vandijk', img: 'imagenes/kanteCap.png'},
+    {id: 4, nombre: 'BECKER', apellido: 'alisson', img: 'imagenes/alissonCap.png'}
+] 
+
+const captainsDiv = document.querySelector('.capitanes-section')
+
+capitanSeleccionado(captainsArray)
+
+function capitanSeleccionado(array){
+    let div = document.createElement('div')
+    div.classList.add('capitanes-container', 'd-flex', 'flex-wrap', 'flex-row', 'align-content-center')
+
+    array.forEach((capitanElegido)=>{
+        div.innerHTML += `
+        <div class="img_caps-container" id="${capitanElegido.apellido}-div">
+            <img src="${capitanElegido.img}" id="${capitanElegido.apellido}" class="img-fluid captain" alt="">
+            <button id="${capitanElegido.id}" class="captainButton"> ELEGIR CAPITAN </button>
+        </div>
+        `
+        captainsDiv.appendChild(div) 
+    })
+
+}
+
+
+/////////////////////////////////////////
+// MANIPULACION DE CAPITANES Y FUNCIONES //
+/////////////////////////////////////////
 
 const captains = document.querySelectorAll('.captain') //llamo todos lo elementos html que compartan la clase .captain
 const captainsArr = Array.from(captains)
 
+console.log(captainsArr)
+
 const containers = document.querySelectorAll('.img_caps-container') //llamo todos los elementos html que compartan la clase img_caps-container
 const containersArr = Array.from(containers)
-
-const captainNewSite = document.querySelector('#capitanSeleccionado')
 
 //con un foreach, recorro cada elemento del array que se crea al llamar a todos los .captain
 captainsArr.forEach(el => {
@@ -104,30 +138,6 @@ captainsArr.forEach(el => {
         captainBenefitDelete.remove()
     })
 
-
-    
-
-    /*
-    //SELECCION DE CAPITAN
-    //EN PROGRESO 
-    el.addEventListener('click', function(){
-        let captainAlert
-        let estructuraCapitan
-
-        switch (el.id) {
-            case 'messi':
-            captainAlert = confirm('¿Deseas elegir a MESSI como CAPITAN?')
-                if (captainAlert == true) {
-                    estructuraCapitan = document.create
-                }
-
-             break
-            
-             case 'ronald o': 
-             captainAlert = confirm
-        }
-    })*/
-
 })
 
 
@@ -136,7 +146,11 @@ captainsArr.forEach(el => {
 
 
 
+
+
+
 //COMPLEMENTARIO EVENTOS!!
+//BOTONES/CREACION Y ELIMINACION DE CAMPO DE JUEGO
     const crear = document.querySelector('#see')
     const nocrear = document.querySelector('#unsee')
     const field = document.querySelector('#field')
---
+++ js/capitanes.js
@@ -0,0 +1,6 @@
+/*let captainsArray = [
+    {id: 1, nombre: 'LIONEL', apellido: 'messi', img: 'imagenes/messiCap.png'},
+    {id: 2, nombre: 'CRISTIANO', apellido: 'ronaldo', img: 'imagenes/ronaldoCap.png'},
+    {id: 3, nombre: 'VIRGIL', apellido: 'vandijk', img: 'imagenes/vandijkCap.png'},
+    {id: 4, nombre: 'BECKER', apellido: 'alisson', img: 'imagenes/alissonCap.png'}
+] */
