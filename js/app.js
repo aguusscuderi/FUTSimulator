a782b7faa16a93df3036fc112c23493cf69a670e
aguusscuderi <aguusscuderi9@gmail.com>
2021-06-18 11:22:47 -0300

    avancessss

--- js/app.js
+++ js/app.js
@@ -249,219 +249,114 @@ captainsArr.forEach(el => {
     formData()
 
 
-    //L.S RECUPERACION USER PLAYERS
+
+    ////////////////////////////////////
+    /// L.S RECUPERACION USER PLAYERS //
+    ///////////////////////////////////
     function recuperacion () { 
         let playersJSON = JSON.parse(localStorage.getItem('jugadores_base'));
         if(playersJSON){
             playersJSON.forEach(el=>{
                jugadoresCreados.push(el) 
-            })
-        }
-    }
-    recuperacion()
-
-
-
-
-
-    ////////////////////////////////
-    // RECUPERACION DE USER CARDS //
-    ///////////////////////////////
-    const card_but = document.getElementById('card_button')
-
-   /* function createCard () {
-    card_but.addEventListener('click', () => {
-        for (let i = 0; i <= 0; i++) {     
-            jugadoresCreados.forEach((el)=> {   
-            const div = document.createElement('div')
-            div.classList.add('card-container')
-            div.innerHTML += `
-           
-                <div class="media-container">
-                    <p> ${el.media} </p>
-                </div>
-                <div class="sep_line1"></div>
-                <div class="nation-container"><img src="imagenes/argentinaIMG.jpg" alt=""></div>
-                <div class="playerPic-container"><img src="imagenes/messiCard.png" alt=""></div>
-                <div class="player-name">
-                    <p>${el.nombre}</p>
-                </div>
-                <div class="player-surname">
-                    <p>${el.apellido}</p>
-                </div>
-                <div class="stats-line-separator"></div>
-                <div class="name-separator"></div>
-                <div class="stats">
-                    <div class="dri">
-                        <p class="value">${el.agilidad}</p>
-                        <p class="name_value">DRI</p>
-                    </div>
-                    <div class="phy">
-                        <p class="value">${el.fuerza}</p>
-                        <p class="name_value">PHY</p>
-                    </div>
-                    <div class="ref">
-                        <p class="value">${el.reflejos}</p>
-                        <p class="name_value">REF</p>
-                    </div>
-                    <div class="def">
-                        <p class="value">${el.velocidad}</p>
-                        <p class="name_value">DEF</p>
-                    </div>
-                </div>
-                <img id="fifa_card" src="imagenes/goldCard.png" alt="">
+               const div = document.createElement('div')
+               div.classList.add('card-container')
+               div.innerHTML += `
           
-            `
-            const userPlayersDiv = document.querySelector('.user-players-div')
-            userPlayersDiv.appendChild(div)
-        })
-        }
-        
-    })
-
-    }*/
-     
-
-
-    /*for (let i = 0; i <= 1; i++) {
-        console.log(i)
-        if(i >= 2){ 
-            alert('Ya recuperaste tus jugadores.')
-        }else{      
-            card_but.addEventListener('click', () => {    
-                console.log(i) 
-                jugadoresCreados.forEach((el)=> {   
-                const div = document.createElement('div')
-                div.classList.add('card-container')
-                div.innerHTML += `       
-                    <div class="media-container">
-                        <p> ${el.media} </p>
-                    </div>
-                    <div class="sep_line1"></div>
-                    <div class="nation-container"><img src="imagenes/argentinaIMG.jpg" alt=""></div>
-                    <div class="playerPic-container"><img src="imagenes/messiCard.png" alt=""></div>
-                    <div class="player-name">
-                        <p>${el.nombre}</p>
-                    </div>
-                    <div class="player-surname">
-                        <p>${el.apellido}</p>
-                    </div>
-                    <div class="stats-line-separator"></div>
-                    <div class="name-separator"></div>
-                    <div class="stats">
-                        <div class="dri">
-                            <p class="value">${el.agilidad}</p>
-                            <p class="name_value">DRI</p>
-                        </div>
-                        <div class="phy">
-                            <p class="value">${el.fuerza}</p>
-                            <p class="name_value">PHY</p>
-                        </div>
-                        <div class="ref">
-                            <p class="value">${el.reflejos}</p>
-                            <p class="name_value">REF</p>
-                        </div>
-                        <div class="def">
-                            <p class="value">${el.velocidad}</p>
-                            <p class="name_value">DEF</p>
-                        </div>
-                    </div>
-                    <img id="fifa_card" src="imagenes/goldCard.png" alt="">
-              
-                `
-                const userPlayersDiv = document.querySelector('.user-players-div')
-                userPlayersDiv.appendChild(div)
-            })
-            
-        }) 
-        }
-        
-    }*/
-
-    //createCard()
-
-
-    for (let i=0; i<=1; i++){
-         card_but.addEventListener('click', () => {    
-            jugadoresCreados.forEach((el)=> {   
-            const div = document.createElement('div')
-            div.classList.add('card-container')
-            div.innerHTML += `       
-                <div class="media-container">
-                    <p> ${el.media} </p>
-                </div>
-                <div class="sep_line1"></div>
-                <div class="nation-container"><img src="imagenes/argentinaIMG.jpg" alt=""></div>
-                <div class="playerPic-container"><img src="imagenes/messiCard.png" alt=""></div>
-                <div class="player-name">
-                    <p>${el.nombre}</p>
-                </div>
-                <div class="player-surname">
-                    <p>${el.apellido}</p>
-                </div>
-                <div class="stats-line-separator"></div>
-                <div class="name-separator"></div>
-                <div class="stats">
-                    <div class="dri">
-                        <p class="value">${el.agilidad}</p>
-                        <p class="name_value">DRI</p>
-                    </div>
-                    <div class="phy">
-                        <p class="value">${el.fuerza}</p>
-                        <p class="name_value">PHY</p>
-                    </div>
-                    <div class="ref">
-                        <p class="value">${el.reflejos}</p>
-                        <p class="name_value">REF</p>
-                    </div>
-                    <div class="def">
-                        <p class="value">${el.velocidad}</p>
-                        <p class="name_value">DEF</p>
-                    </div>
-                </div>
-                <img id="fifa_card" src="imagenes/goldCard.png" alt="">
-          
-            `
-            const userPlayersDiv = document.querySelector('.user-players-div')
-            userPlayersDiv.appendChild(div)
-        })
-        
-    })
-    }
-
-
-//////////////////////////////////////////////////////
-// BOTONES/CREACION Y ELIMINACION DE CAMPO DE JUEGO //
-//////////////////////////////////////////////////////
-    const crear = document.querySelector('#see')
-    const nocrear = document.querySelector('#unsee')
-    const field = document.querySelector('#field')
-    const fieldFirstPosition = document.querySelector('.positionsDivContainer')
-
-    let creado = 'Creaste un elemento';
-    const arrayContador = []
-    
-    nocrear.addEventListener('click', function(){
-        arrayContador.shift()
-        console.log(arrayContador.length)
-        field.remove()
-        const newFieldRemove = document.querySelector('.recreatedField')
-        newFieldRemove.remove()
-    })
+               <div class="media-container">
+                   <p> ${el.media} </p>
+               </div>
+               <div class="sep_line1"></div>
+               <div class="nation-container"><img src="imagenes/argentinaIMG.jpg" alt=""></div>
+               <div class="playerPic-container"><img src="imagenes/messiCard.png" alt=""></div>
+               <div class="player-name">
+                   <p>${el.nombre}</p>
+               </div>
+               <div class="player-surname">
+                   <p>${el.apellido}</p>
+               </div>
+               <div class="stats-line-separator"></div>
+               <div class="name-separator"></div>
+               <div class="stats">
+                   <div class="dri">
+                       <p class="value">${el.agilidad}</p>
+                       <p class="name_value">DRI</p>
+                   </div>
+                   <div class="phy">
+                       <p class="value">${el.fuerza}</p>
+                       <p class="name_value">PHY</p>
+                   </div>
+                   <div class="ref">
+                       <p class="value">${el.reflejos}</p>
+                       <p class="name_value">REF</p>
+                   </div>
+                   <div class="def">
+                       <p class="value">${el.velocidad}</p>
+                       <p class="name_value">DEF</p>
+                   </div>
+               </div>
+               <img id="fifa_card" src="imagenes/goldCard.png" alt="">
+         
+           `
+           const userPlayersDiv = document.querySelector('.user-players-div')
+           userPlayersDiv.appendChild(div)
+           })
+       }
+   }
+   recuperacion()
 
 
 
-    crear.addEventListener('click', function(){
-        
-        arrayContador.push(creado)
-        console.log(arrayContador.length)
-        let fieldPosition = document.createElement('div')
-        fieldPosition.setAttribute('class', 'recreatedField')
-        fieldPosition.innerHTML += `
-        <img src="imagenes/planillaFutbol.jpg" id="field" class="img-fluid" alt="">
+
+
+    ////////////////////////////////////////
+    // AGREGANDO JUGADORES A LA CANCHITA //
+    //////////////////////////////////////
+
+
+ //const jugadoresCreados = [] (CORRESPONDE A LA VARIABLE QUE CONTIENE LOS JUGADORES CREADOS.)   
+
+const jugadorStandard = document.querySelectorAll('.jugador-standard')
+const containerPlayer = document.querySelectorAll('.player__item')
+
+//AGREGANDO LOS JUGADORES STANDARD
+function addPlayer (){
+    jugadorStandard.forEach(el => {
+    el.addEventListener('click', () => {
+        let jugadorStandardSRC = el.currentSrc
+        const div = document.createElement('div')
+        div.innerHTML += `
+            <img class="img_chsnPlayer" src="${jugadorStandardSRC}"/>
         `
-        fieldFirstPosition.appendChild(fieldPosition)  
+        for (let pos of containerPlayer){
+            if(pos.children.length == 0){
+                if(el.classList[0] == 'delantero' && (pos.id == 11 || pos.id == 10 || pos.id == 9)){
+                    pos.appendChild(div)
+                }else if (el.classList[0] == 'midfielder' && (pos.id == 8 || pos.id == 7 || pos.id == 5)){
+                    pos.appendChild(div)
+                }else if(el.classList[0] == 'defensor' && (pos.id == 6 || pos.id == 4 || pos.id == 3 || pos.id == 2)){
+                    pos.appendChild(div)
+                }else if(el.classList[0] == 'goalkeeper' && pos.id == 1){
+                    pos.appendChild(div)
+                }
+            }
+          
+        }
+    })   
 })
+
+}
+
+addPlayer()
+
+ /* const random = Math.floor(Math.random() * containerPlayer.length);
+        let randomPosition = containerPlayer[random]*/
+
+
+
+
+
+
+
 
 
 
